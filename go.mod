d3b82d0c47d2acd5b7fc2a12089c806374cd2819
Giridhar Bhujanga <gbhujanga@walmartlabs.com>
2020-12-17 13:03:23 -0800

    generated new mod files after upgrading to go version go1.15.6

---
+++ go.mod
@@ -0,0 +1,8 @@
+module udemy/grpc/giridhar/client-streaming/server
+
+go 1.14
+
+require (
+	github.com/giridharmb/grpc-messagepb v0.0.0-20201217203313-0010de533b23
+	google.golang.org/grpc v1.34.0
+)
--- main.go
+++ main.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/giridharmb/grpc-messagepb"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"strconv"
+	"time"
+)
+
+type server struct{}
+
+func (s *server) FetchData(request *messagepb.Request, stream messagepb.MyDataService_FetchDataServer) error {
+	fmt.Printf("\nFetchData was invoked, this will now stream data back to client...")
+	myData := request.GetData()
+	firstName := myData.GetFirstName()
+	lastName := myData.GetLastName()
+	age := myData.GetAge()
+	for i := 0; i < 10; i++ {
+		result := "Hello " + firstName + " , " + lastName + " whose age is " + strconv.Itoa(int(age)) + " : counter => " + strconv.Itoa(i)
+		responseResult := &messagepb.Response{Result: result}
+		_ = stream.Send(responseResult)
+		fmt.Printf("\nstream.Send...")
+		time.Sleep(1000 * time.Millisecond)
+	}
+	return nil
+}
+
+func (*server) GetSum(ctx context.Context, req *messagepb.SumRequest) (*messagepb.SumResponse, error) {
+	fmt.Printf("\nReceived Sum Request : %v", req)
+	num1 := req.NumberFirst
+	num2 := req.NumberSecond
+
+	total := num1 + num2
+	result := &messagepb.SumResponse{SumResult: total}
+	return result, nil
+}
+
+func main() {
+	fmt.Printf("\nserver ...")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen : %v", err)
+	}
+
+	myServer := grpc.NewServer()
+
+	messagepb.RegisterMyDataServiceServer(myServer, &server{})
+	if err := myServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve : %v", err)
+	}
+}
